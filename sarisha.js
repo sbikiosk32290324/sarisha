1bb0dabc5d022d6b860795118e2c952e47b51afe
sbikiosk <164787041+sbikiosk32290324@users.noreply.github.com>
2025-03-07 23:22:22 +0530

    Update sarisha.js

--- sarisha.js
+++ sarisha.js
@@ -5,57 +5,39 @@ function multiply()
       var qnt=document.getElementById("Quantity1").value;
         var amt=final_data*qnt;
       document.getElementById("total1").value=amt;
-	  var note2= document.getElementById("note2").value;
-      var qnt2=document.getElementById("Quantity2").value;
-        var amt2=note2*qnt2;
-      document.getElementById("total2").value=amt2;
+
 	  var note3= document.getElementById("note3").value;
       var qnt3=document.getElementById("Quantity3").value;
         var amt3=note3*qnt3;
       document.getElementById("total3").value=amt3;
-	  var note4= document.getElementById("note4").value;
-      var qnt4=document.getElementById("Quantity4").value;
-        var amt4=note4*qnt4;
-      document.getElementById("total4").value=amt4;
+	 
 	  var note5= document.getElementById("note5").value;
       var qnt5=document.getElementById("Quantity5").value;
         var amt5=note5*qnt5;
       document.getElementById("total5").value=amt5;
 	  
-	  var note6= document.getElementById("note6").value;
-      var qnt6=document.getElementById("Quantity6").value;
-        var amt6=note6*qnt6;
-      document.getElementById("total6").value=amt6;
+	
 	  
 	  var note7= document.getElementById("note7").value;
       var qnt7=document.getElementById("Quantity7").value;
         var amt7=note7*qnt7;
       document.getElementById("total7").value=amt7;
 	  
-	  var note8= document.getElementById("note8").value;
-      var qnt8=document.getElementById("Quantity8").value;
-        var amt8=note8*qnt8;
-      document.getElementById("total8").value=amt8;
+	
 	  
 	  var note9= document.getElementById("note9").value;
       var qnt9=document.getElementById("Quantity9").value;
         var amt9=note9*qnt9;
       document.getElementById("total9").value=amt9;
 	  
-	  var note10= document.getElementById("note10").value;
-      var qnt10=document.getElementById("Quantity10").value;
-        var amt10=note10*qnt10;
-      document.getElementById("total10").value=amt10;
+	
 	  
 	  var note11= document.getElementById("note11").value;
       var qnt11=document.getElementById("Quantity11").value;
         var amt11=note11*qnt11;
       document.getElementById("total11").value=amt11;
 	  
-	  var note12= document.getElementById("note12").value;
-      var qnt12=document.getElementById("Quantity12").value;
-        var amt12=note12*qnt12;
-      document.getElementById("total12").value=amt12;
+	
 	  
 	  var note13= document.getElementById("note13").value;
       var qnt13=document.getElementById("Quantity13").value;
@@ -76,94 +58,25 @@ function multiply()
       var qnt16=document.getElementById("Quantity16").value;
         var amt16=note16*qnt16;
       document.getElementById("total16").value=amt16;
-	  
-	  var note17= document.getElementById("anup").value;
-	  var amt17=note17
-	  document.getElementById("total17").value=amt17;
+	
 	  
 	  var total1= document.getElementById("total1").value;
-	  var total2= document.getElementById("total2").value;
-	  var total3= document.getElementById("total3").value;
-	  var total4= document.getElementById("total4").value;
-	  var total5= document.getElementById("total5").value;
-	  var total6= document.getElementById("total6").value;
-	  var total7= document.getElementById("total7").value;
-	  var total8= document.getElementById("total8").value;
-	  var total9= document.getElementById("total9").value;
-	  var total10= document.getElementById("total10").value;
-	  var total11= document.getElementById("total11").value;
-	  var total12= document.getElementById("total12").value;
-	  var total13= document.getElementById("total13").value;
-	  var total14= document.getElementById("total14").value;
-	  var total15= document.getElementById("total15").value;
-	  var total16= document.getElementById("total16").value;
-	  var total17= document.getElementById("total17").value;
-	  var total18= document.getElementById("total18").value;
-	  var total19= document.getElementById("total19").value;
-	 
-	  var total21= document.getElementById("total21").value;
-	  var total22= document.getElementById("total22").value;
-	  var total23= document.getElementById("total23").value;
-	  var total24= document.getElementById("total24").value;
-	  var total25= document.getElementById("total25").value;
+	    var total1= document.getElementById("total3").value;
+	    var total1= document.getElementById("total5").value;
+	    var total1= document.getElementById("total7").value;
+	    var total1= document.getElementById("total9").value;
+	    var total1= document.getElementById("total11").value;
+	    var total1= document.getElementById("total13").value;
+	    var total1= document.getElementById("total14").value;
+	    var total1= document.getElementById("total15").value;
+	    var total1= document.getElementById("total16").value;
+	  
 	  
 	 
-	  var all1=+(total1)+ +(total3)+ +(total5)+ +(total7)+ +(total9)+ +(total11)+ +(total13)+ +(total15)+ +(total14)+ +(total16)+ +(total21)+ +(total23);
-	  document.getElementById("total26").value=all1;
+	
 	  
-	  var qt20=+(qnt2)+ +(qnt4)+ +(qnt6)+ +(qnt8)+ +(qnt10)+ +(qnt12);
-	  document.getElementById("non").value=qt20;
-	  
-	  var qtsit1=+(qnt)+ +(qnt3)+ +(qnt5)+ +(qnt7)+ +(qnt9)+ +(qnt11)+ +(qnt13)+ +(qnt15)+ +(qnt14)+ +(qnt16);
-	  document.getElementById("od").value=qtsit1;
-	  
-	  var amt20=+(total2)+ +(total4)+ +(total6)+ +(total8)+ +(total10)+ +(total12)+ +(total17)+ +(total19)+ +(total18)+ +(total22)+ +(total24)+ +(total25);
-	  document.getElementById("total20").value=amt20;
-	  
-	  var amtg=+(total2)+ +(total4)+ +(total6)+ +(total8)+ +(total10)+ +(total12)+ +(total17)+ +(total19)+ +(total18)+ +(total22)+ +(total24)+ +(total25)+
-+(total1)+ +(total3)+ +(total5)+ +(total7)+ +(total9)+ +(total11)+ +(total13)+ +(total15)+ +(total14)+ +(total16)+ +(total21)+ +(total23)	  ;
+	  var amtg=+(total1)+ +(total3)+ +(total5)+ +(total7)+ +(total9)+ +(total11)+ +(total13)+ +(total15)+ +(total14)+ +(total16)  ;
 	  document.getElementById("all_total").value=amtg;
 	  
 	  
-	  var note18= document.getElementById("dipika").value;
-	  var amt18=note18
-	  document.getElementById("total18").value=amt18;
-	  
-	  var note21= document.getElementById("bc").value;
-	  var amt21=note21
-	  document.getElementById("total21").value=amt21;
-	  
-	  var note22= document.getElementById("exc").value;
-	  var amt22=note22
-	  document.getElementById("total22").value=amt22;
-	  
-	  var note23= document.getElementById("ac").value;
-	  var amt23=note23
-	  document.getElementById("total23").value=amt23;
-	  
-	  var note24= document.getElementById("ofe").value;
-	  var amt24=note24
-	  document.getElementById("total24").value=amt24;
-	  
-	  var note25= document.getElementById("pc").value;
-	  var amt25=note25
-	  document.getElementById("total25").value=amt25;
-	  
-	  
-	  
-	  
-	  var note19= document.getElementById("loan").value;
-	  var amt19=note19
-	  document.getElementById("total19").value=amt19;
-	  
-	  var ato= document.getElementById("all_total").value;
-	  var pob= document.getElementById("total26_1").value;
-	  var mib=-(pob)- -(ato) 
-	  document.getElementById("db").value=mib;
-	  
-	  
-
-
-
-document.getElementById("Quantity1").max = "10";
-	  }
+	 
